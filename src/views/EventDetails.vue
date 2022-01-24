e7b4f5622eff228674f4899c5b00ab6a75f3b7ee
BiasedLiar <Bretagard12345>
2022-01-24 13:28:31 +0100

    adding files to github

---
+++ src/views/EventDetails.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="event">
+      <h1>{{ event.title}}</h1>
+      <p>{{event.time}} at {{event.date}} @ {{event.place}}</p>
+      <p>{{event.description}}</p>
+  </div>
+</template>
+
+<script>
+import EventServices from '@/services/EventServices.js'
+export default {
+    props: ['id'],
+    data(){
+        return{
+            event: null
+        }
+    },
+    created(){
+        EventServices.getEvent(this.id)
+      .then(response =>
+        this.event = response.data)
+      .catch(error =>
+        console.log(error))
+    }
+
+}
+</script>
+
+<style>
+
+</style>
